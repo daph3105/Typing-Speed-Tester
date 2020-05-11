52d0e57d8a755c43418fe3d26ca6ad2e214f44a1
Daphne <daphne.feresin@gmail.com>
2020-05-11 08:09:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
+
+// Add leading zero to numbers 9 or below (purely for aesthetics):
+
+
+// Run a standard minute/second/hundredths timer:
+
+
+// Match the text entered with the provided text on the page:
+
+
+// Start the timer:
+function start (){
+    let textEnteredLength = testArea.value.length;
+    console.log(textEnteredLength)
+}
+
+function spellCheck (){
+    let textEntered = testArea.value;
+    console.log(textEntered)
+}
+
+function reset(){
+    console.log("reset button has been pressed");
+}
+
+
+// Reset everything:
+
+
+// Event listeners for keyboard input and the reset button:
+
+testArea.addEventListener('keypress', start, false)
+
+// keyup = event happens when you let go of the key
+testArea.addEventListener('keyup', spellCheck,false);
+resetButton.addEventListener('click', reset, false);
